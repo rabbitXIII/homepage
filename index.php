b34ec3845f348f2e34d2a708ef1db9f70e9b277a
Rohit Gopal <rohit@collegerail.com>
2013-08-13 19:22:28 +0000

    added links, and profile picture. added a stylesheet

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php 
 $title = "Rohit Gopal's Profile";
-echo "<title>$title</title>";
-echo "This is Rohit's Page";
 ?>
+<head>
+	<link rel="stylesheet" type="text/css" href="style/index.css" />
+</head>
+<body>
+	<title><?php echo $title;?></title>
+	<h1>Rohit's Homepage</h1>
+	<div id="profile_pic" class="frame">
+		<img src="images/rohit_prof.jpg" alt="Rohit Gopal" />
+	</div>
+	<br />
+	<h2>Papers / Publications</h2>
+	<ul>
+		<li><a href="http://onlinelibrary.wiley.com/doi/10.1002/sat.973/abstract">Cross-plane information sharing for QoS in satellite-terrestrial integrated packet networks</a></li>
+		<li><a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=4753517&url=http%3A%2F%2Fieeexplore.ieee.org%2Fxpls%2Fabs_all.jsp%3Farnumber%3D4753517">Framework for rapid deployment of rule-based situation management systems</a></li>
+		<li><a href="http://patent.ipexl.com/U2S/20080046387.html">System and method for policy based control of local electrical energy generation and use</a></li>
+		<li><a href="http://www.ncbi.nlm.nih.gov/pubmed/20237037">Prognostic implication of dual-phase PET in adenocarcinoma of the lung</a></li>
+		<li><a href="http://www.ncbi.nlm.nih.gov/pubmed/17418151">Structural and functional imaging of normal bone marrow and evaluation of its age-related changes</a></li>
+	</ul>
+	<h2>Links</h2>
+	<ul>
+		<li><a href="http://www.linkedin.com/pub/rohit-gopal/12/2a2/977">LinkedIn</a></li>
+		<li>Previous Work 
+			<ul>
+				<li><a href="http://www.wedotechnologies.com/en/">WeDo Technologies</a></li>	
+				<li><a href="http://www.cattlab.umd.edu/">CATT Lab</a></li>	
+				<li><a href="http://www.uphs.upenn.edu/radiology/">Hospital of UPenn Dept. of Radiology</a></li>	
+			</ul>
+		</li>
+	</ul>
+</body>
+</html>
