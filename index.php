d51227edce7e0961df63a42769dbb8aea7114384
Rohit Gopal <rohit@gopals.us>
2014-04-21 22:26:57 +0000

    changed link for patent

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $title = "Rohit Gopal's Profile";
 	<ul>
 		<li><a href="http://onlinelibrary.wiley.com/doi/10.1002/sat.973/abstract">Cross-plane information sharing for QoS in satellite-terrestrial integrated packet networks</a></li>
 		<li><a href="http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=4753517&url=http%3A%2F%2Fieeexplore.ieee.org%2Fxpls%2Fabs_all.jsp%3Farnumber%3D4753517">Framework for rapid deployment of rule-based situation management systems</a></li>
-		<li><a href="http://patent.ipexl.com/U2S/20080046387.html">System and method for policy based control of local electrical energy generation and use</a></li>
+		<li><a href="http://www.google.com/patents/US20080046387">System and method for policy based control of local electrical energy generation and use</a></li>
 		<li><a href="http://www.ncbi.nlm.nih.gov/pubmed/20237037">Prognostic implication of dual-phase PET in adenocarcinoma of the lung</a></li>
 		<li><a href="http://www.ncbi.nlm.nih.gov/pubmed/17418151">Structural and functional imaging of normal bone marrow and evaluation of its age-related changes</a></li>
 	</ul>
